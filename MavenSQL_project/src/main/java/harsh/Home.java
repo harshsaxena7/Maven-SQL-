a5b24bb0f0324f78b13c302985f1b0907ea0843a
Harsh Saxena <98602942+harshsaxena7@users.noreply.github.com>
2024-08-01 22:35:14 +0530

    Update Home.java

--- MavenSQL_project/src/main/java/harsh/Home.java
+++ MavenSQL_project/src/main/java/harsh/Home.java
@@ -34,7 +34,7 @@ public class Home extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
